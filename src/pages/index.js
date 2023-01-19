1db2943ef11c4e78dcb654ac2bec3633aa040322
Dima <dsuhih355@gmail.com>
2023-01-19 20:45:42 +0800

    image master

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image"
 
 
 // Step 2: Define your component
@@ -8,7 +9,10 @@ const IndexPage = () => {
 return (
 <Layout PageHeading='Домашняя' PageTitle='Ремонт стиральных машин'>
     Добро пожаловать на нашу главную страницу
+    <StaticImage src="../images/master.jpg" alt="мастер ремонтирует холодильник" />
+     <StaticImage src="../images/refrigeratorrepair.webp" alt="мастер ремонтирует холодильник" />
 </Layout>
+
 )
 }
 
